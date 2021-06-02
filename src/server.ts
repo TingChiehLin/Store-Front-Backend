6ca28182fff9e14bebe9517430503c9243a0775d
Jay Lin <cooloojayoo@gmail.com>
2021-06-02 15:07:31 +1000

    Test

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response, Application } from 'express'
 import bodyParser from 'body-parser'
 
+import user_list_routes from "./controllers/user_handler";
 import product_list_routes from "./controllers/product_handler";
+import order_list_routes from "./controllers/order_handler";
 
 const app: express.Application = express()
 const router = express()
@@ -17,7 +19,9 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Welcome Store Front!')
 })
 
+user_list_routes(app)
 product_list_routes(app)
+order_list_routes(app)
 
 const server = app.listen(8080, function () {
     console.log(`starting app on: ${address}`)
